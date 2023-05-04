6ce556f88710a72ca7b769dedff4e07f73a0483c
Fue Lee <fueclee.12@gmail.com>
2023-05-04 10:52:07 -0500

    enums and models set as a group

--- Models/PetOwner.cs
+++ Models/PetOwner.cs
@@ -5,5 +5,21 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace pet_hotel
 {
-    public class PetOwner {}
+    public class PetOwner
+    {
+        public int id {get; set;}
+        [Required]
+        public string name {get; set;}
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string emailAddress { get; set; }
+        [NotMapped]
+        public int PetCount => Pets.Count;
+        public List<Pet> Pets {get; set;}
+        public PetOwner()
+        {
+            Pets = new List<Pet>();
+        }
+    }
 }
